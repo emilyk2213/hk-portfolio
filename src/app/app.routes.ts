9bc2ea4a3344df0dbb5cd95ffdab6e1a74764d6e
Emily <emily.kappen@gmail.com>
2026-01-03 14:59:00 +0200

    Routing and file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/portfolio/portfolio')
+        .then(m => m.Portfolio)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about')
+        .then(m => m.About)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/contact')
+        .then(m => m.Contact)
+  }
+];
